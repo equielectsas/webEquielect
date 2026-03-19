ca4d0d5ee31082d3518e62abbf3d5a4ae24357e5
Jeronimo Lopez Montoya <jerolomon@gmail.com>
2026-03-19 11:32:14 -0500

    arreglo analytics

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_GA_TRACKING_ID: process.env.NEXT_PUBLIC_GA_TRACKING_ID,
-  },
   images: {
     domains: [
       "th.bing.com",
